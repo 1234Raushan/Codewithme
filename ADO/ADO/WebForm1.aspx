750253ecf6b0fedc5d9327d2ab02f7c49a848a65
Raushan Manjhi <Raushan.manjhi@mycom-osi.com>
2023-05-22 22:13:28 +0530

    <Lab-1> added html and css as well as ADO.net project

---
+++ ADO/ADO/WebForm1.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ADO.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 1492px;
+            height: 380px;
+        }
+        .auto-style2 {
+            position: relative;
+            top: 50px;
+            left: 521px;
+            z-index: 1;
+        }
+        .auto-style3 {
+            position: absolute;
+            top: 139px;
+            left: 589px;
+            z-index: 1;
+        }
+        .auto-style4 {
+            width: 289px;
+            height: 180px;
+            position: absolute;
+            top: 230px;
+            left: 457px;
+            z-index: 1;
+        }
+        .auto-style5 {
+            position: relative;
+            top: 51px;
+            left: 98px;
+            z-index: 1;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="auto-style1">
+            <asp:TextBox ID="TextBox1" runat="server" CssClass="auto-style2" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" CssClass="auto-style3" OnClick="Button1_Click" Text="Save" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Name<asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" CssClass="auto-style4" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
+                <Columns>
+                    <asp:BoundField DataField="Personid" HeaderText="Personid" />
+                    <asp:BoundField DataField="Name" HeaderText="Name" />
+                    <asp:BoundField DataField="Data" HeaderText="Data" />
+                </Columns>
+            </asp:GridView>
+            <asp:TextBox ID="TextBox2" runat="server" CssClass="auto-style5" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+            data</div>
+    </form>
+</body>
+</html>
---
+++ ADO/ADO/WebForm1.aspx.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.html.simpleparser;
+using System.Web.UI.WebControls;
+using System.Reflection.Metadata;
+
+namespace ADO
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=IN-RMANJHI-T14\MSSQLSERVER1;Initial Catalog=employeeDb;Integrated Security=True");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter da = new SqlDataAdapter();
+        //DataTable dataTable = new DataTable();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string companyName = "Nemboolisoft";
+            int orderNo = 1234;
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[3] {
+                            new DataColumn("Personid", typeof(string)),
+                            new DataColumn("Name", typeof(string)),
+                            new DataColumn("Data", typeof(int)),
+                            });
+            //dt.Rows.Add(101, "Sun Glasses", 200, 5, 1000);
+            //dt.Rows.Add(102, "Jeans", 400, 2, 800);
+            //dt.Rows.Add(103, "Trousers", 300, 3, 900);
+            //dt.Rows.Add(104, "Shirts", 550, 2, 1100);
+            using (StringWriter sw = new StringWriter())
+            {
+                using (HtmlTextWriter hw = new HtmlTextWriter(sw))
+                {
+                    StringBuilder sb = new StringBuilder();
+
+                    //Generate Invoice (Bill) Header.
+                    sb.Append("<table width='100%' cellspacing='0' cellpadding='2'>");
+                    sb.Append("<tr><td align='center' style='background-color: #18B5F0' colspan = '2'><b>Order Sheet</b></td></tr>");
+                    sb.Append("<tr><td colspan = '2'></td></tr>");
+                    sb.Append("<tr><td><b>Order No: </b>");
+                    sb.Append(orderNo);
+                    sb.Append("</td><td align = 'right'><b>Date: </b>");
+                    sb.Append(DateTime.Now);
+                    sb.Append(" </td></tr>");
+                    sb.Append("<tr><td colspan = '2'><b>Company Name: </b>");
+                    sb.Append(companyName);
+                    sb.Append("</td></tr>");
+                    sb.Append("</table>");
+                    sb.Append("<br />");
+
+                    //Generate Invoice (Bill) Items Grid.
+                    sb.Append("<table border = '1'>");
+                    sb.Append("<tr>");
+                    foreach (DataColumn column in dt.Columns)
+                    {
+                        sb.Append("<th style = 'background-color: #D20B0C;color:#ffffff'>");
+                        sb.Append(column.ColumnName);
+                        sb.Append("</th>");
+                    }
+                    sb.Append("</tr>");
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        sb.Append("<tr>");
+                        foreach (DataColumn column in dt.Columns)
+                        {
+                            sb.Append("<td>");
+                            sb.Append(row[column]);
+                            sb.Append("</td>");
+                        }
+                        sb.Append("</tr>");
+                    }
+                    sb.Append("<tr><td align = 'right' colspan = '");
+                    sb.Append(dt.Columns.Count - 1);
+                    sb.Append("'>Total</td>");
+                    sb.Append("<td>");
+                    sb.Append(dt.Compute("sum(Total)", ""));
+                    sb.Append("</td>");
+                    sb.Append("</tr></table>");
+
+                    //Export HTML String as PDF.
+                    StringReader sr = new StringReader(sb.ToString());
+                    Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 10f, 0f);
+                    HTMLWorker htmlparser = new HTMLWorker(pdfDoc);
+                    PdfWriter writer = PdfWriter.GetInstance(pdfDoc, Response.OutputStream);
+                    pdfDoc.Open();
+                    htmlparser.Parse(sr);
+                    pdfDoc.Close();
+                    Response.ContentType = "application/pdf";
+                    Response.AddHeader("content-disposition", "attachment;filename=Invoice_" + orderNo + ".pdf");
+                    Response.Cache.SetCacheability(HttpCacheability.NoCache);
+                    Response.Write(pdfDoc);
+                    Response.End();
+                }
+            }
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+                 
+            string query = "insert into dbo.Persons(Name,Data) values('" + TextBox1.Text + "','" + TextBox2.Text + "')";
+            SqlCommand sc = new SqlCommand(query, con); 
+            con.Open();
+            int status = sc.ExecuteNonQuery();
+
+            SqlDataAdapter sde = new SqlDataAdapter("Select * from dbo.Persons", con);
+            DataSet ds = new DataSet();
+            sde.Fill(ds);
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
